3a79ab78bb74cf3d82682d6cc4f5a112b605ea5b
jerrie cheng <jerriecheng@qnap.com>
2024-03-11 03:48:05 +0000

    update

--- warehouse_docker/api/django_warehouse/settings.py
+++ warehouse_docker/api/django_warehouse/settings.py
@@ -189,5 +189,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'kyotozuiryu@gmail.com'
-EMAIL_HOST_PASSWORD = 'uvuhkqnqxqfdposi'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
